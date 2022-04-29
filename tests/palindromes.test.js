84e12a9aa0eae35a7557a4f4bdeb2bb96a743d0b
crochetcode <fred.davison@outlook.com>
2022-04-29 17:55:10 -0500

    :wrench: initial prettier setup

--- tests/palindromes.test.js
+++ tests/palindromes.test.js
@@ -3,5 +3,6 @@ const palidromes = require('../src/palindromes');
 describe('palindromes', () => {
     it('correctly identifies one-word palindromes', () => {
         expect(palidromes('madam')).toEqual(['madam']);
+        expect(palidromes('racecar')).toEqual(['racecar']);
     });
 });
