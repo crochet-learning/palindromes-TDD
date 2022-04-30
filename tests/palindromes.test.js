af2f9f50e3c79f37d65a0dafb050eba826553943
crochetcode <fred.davison@outlook.com>
2022-04-30 14:09:28 -0500

    WIP: set up tests

--- tests/palindromes.test.js
+++ tests/palindromes.test.js
@@ -5,4 +5,18 @@ describe('palindromes', () => {
     expect(palidromes('madam')).toEqual(['madam']);
     expect(palidromes('racecar')).toEqual(['racecar']);
   });
+
+  it('ignores casing', () => {
+    expect(palidromes('WoW')).toEqual(['wow']);
+  });
+
+  it('ignores punctuation', () => {
+    expect(palidromes('yo, banana boy!')).toEqual(['yobananaboy']);
+  });
+
+  it('detects multi-word palindromes', () => {
+    expect(palidromes('A man, a plan, a canal, Panama')).toEqual([
+      'amanaplanacanalpanama',
+    ]);
+  });
 });
